25860a583e65b88690c00d361c4c1b99fdf09202
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-02-22 23:06:39 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 //Elena Voinu
 public class Main {
     public static void main(String[] args) {
+        
         int [] array = {12, 5, 67, 3, 4, 9};
+        //search in the array for number 9.
         int result = linearSearch(array, 9);
 
         if(result == -1){
@@ -14,11 +16,15 @@ public class Main {
     // array to be searched, num = the value we are looking for
     public static int linearSearch(int[]array, int num)
     {
+        //loop through the array 
         for(int i = 0; i < array.length; i++){
+            //if number found 
             if(array[i] == num) {
+                //return the index it's at
                 return (i);
             }
         }
+        // return -1 when number is not found in the array
         return -1;
 
     }
